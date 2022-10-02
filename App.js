b5f78da2e4428241900c1bd95d579a483b4f4b5d
Alex Maskeny <alex@maskeny.com>
2022-10-02 12:46:39 -0400

    Added authentication and necessary aws and other dependenacies.

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import { Amplify } from 'aws-amplify';
 
-export default function App() {
+import awsconfig from './src/aws-exports';
+
+Amplify.configure(awsconfig)
+
+export default withAuthenticator(function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -9,7 +15,7 @@ export default function App() {
           <Text>Alex Maskeny</Text>
     </View>
   );
-}
+})
 
 const styles = StyleSheet.create({
   container: {
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "proxychat33e582f8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "proxychat": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
