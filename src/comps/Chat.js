a8d3b8030bb94112569c9d08501b6deb7757244d
Alex Maskeny <alex@maskeny.com>
2022-10-15 20:20:23 -0400

    Save to test reimplemntation of the ChatPage to fix KeyBoardAvoidingView problem.

--- src/comps/Chat.js
+++ src/comps/Chat.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, StyleSheet, ImageBackground, FlatList } from 'react-native';
-import { useKeyboard } from '@react-native-community/hooks';
 
 import { colors, css } from '../config';
 import BeamTitle from './BeamTitle';
@@ -22,7 +21,6 @@ function Chat({
     navigation,
     ...props
 }) {
-    const Keyboard = useKeyboard();
     return (
         <View style={styles.container}>
             <ImageBackground
@@ -85,7 +83,7 @@ function Chat({
                         marginTop: -60,
                         alignSelf: "center",
                     }}
-                    onPress={() => navigation.navigate("ChatPage", { name: title, KeyboardHeight: Keyboard.keyboardHeight })}
+                    onPress={() => navigation.navigate("ChatPage", { name: title })}
                     
                 />
             </View>
--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,9 +1,8 @@
-import { useKeyboard } from '@react-native-community/hooks';
 import React from 'react';
-import { StyleSheet, Image, ActivityIndicator, View, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, Image, ActivityIndicator, FlatList, View, KeyboardAvoidingView } from 'react-native';
 import IconButton from '../comps/IconButton';
 
-import {colors,css } from '../config'
+import {colors,css,debug } from '../config'
 import Screen from '../comps/Screen';
 import SimpleButton from '../comps/SimpleButton';
 import SimpleInput from '../comps/SimpleInput';
@@ -13,69 +12,150 @@ import SimpleMessage from '../comps/SimpleMessage';
 //DESCRIPTION: A primary page of the SecondaryNav
 //             is the hub for all localized chats
 
+//REMOVE ON PRODUCTON [START]
+//!!!!!!
+//!!!!!!
+//!!!!!!
+const chats = [
+    {
+        id: "1",
+        username: "Alexander",
+        message: "Hey this is alex, what's up?"
+    },
+    {
+        id: "2",
+        username: "Alexander",
+        message: "Hey this is alex, what's up?"
+    },
+    {
+        id: "3",
+        username: "Alexander",
+        message: "Hey this is alex, what's up?"
+    },
+    {
+        id: "4",
+        username: "Alexander",
+        message: "Hey this is alex, what's up?"
+    },
+    {
+        id: "5",
+        username: "Alexander",
+        message: "Hey this is alex, what's up?"
+    },
+    {
+        id: "6",
+        username: "Alexander",
+        message: "Hey this is alex, what's up?"
+    },
+    {
+        id: "7",
+        username: "Alexander",
+        message: "Hey this is alex, what's up?"
+    },
+    {
+        id: "8",
+        username: "Alexander",
+        message: "Hey this is alex, what's up?"
+    },
+    {
+        id: "9",
+        username: "Alexander",
+        message: "Hey this is alex, what's up?"
+    },
+    {
+        id: "10",
+        username: "Alexander",
+        message: "Hey this is alex, what's up?"
+    },
+    {
+        id: "11",
+        username: "Alexander",
+        message: "Hey this is alex, what's up?"
+    },
+    {
+        id: "12",
+        username: "Alexander",
+        message: "Hey this is alex, what's up?"
+    },
+    {
+        id: "13",
+        username: "Alexander",
+        message: "Hey this is alex, what's up?"
+    },
+]
+//!!!!!!
+//!!!!!!
+//!!!!!!
+//REMOVE ON PRODUCTON [END]
+
 function ChatPage({ route }) {
     const [msg, setMsg] = React.useState("");
     const msgRef = React.useRef();
-    const Keyboard = useKeyboard();
-    //React.useEffect(() => {
-    //    console.log(route.params.KeyboardHeight)
-    //},[])
     return (
         <Screen innerStyle={styles.page}>
-            <View style={styles.chats}>
-                <SimpleMessage
-                    ppic={{ uri: 'https://alexmaskeny.com/wp-content/uploads/2020/11/dragonbackground.jpg' }}
-                    username="Alexander"
-                    message="Hey this is alex, what's up?"
-                />
+            <KeyboardAvoidingView style={{ flex: 1, justifyContent: "flex-end" }} behavior="padding">
+                <View style={styles.chats}>
+                    <FlatList
+                        data={chats}
+                        keyExtractor={item => item.id}
+                        renderItem={({ item }) => (
+                            <View style={{margin: 6} }>
+                                <SimpleMessage
+                                    ppic={{ uri: 'https://alexmaskeny.com/wp-content/uploads/2020/11/dragonbackground.jpg' }}
+                                    username={item.username}
+                                    message={item.message}
+                                />
+                             </View>
+                        )}
+                    />
 
-            </View>
-            <DarkBeam style={{ backgroundColor: colors.container, height: 1, marginBottom: 10}} />
-            <View style={styles.textBox}>
-                <IconButton
-                    icon="camera"
-                    brand="Ionicons"
-                    color={colors.text3}
-                    style={{ marginBottom: 6, }}
-                    size={34}
-                    onPress={() => { if (debug) console.log("Open Camera") }}
-                />
-                <View style={{width: 10} } />
-                <IconButton
-                    icon="add-circle"
-                    brand="Ionicons"
-                    color={colors.text3}
-                    size={34}
-                    style={{ marginBottom: 6, } }
-                    onPress={() => { if (debug) console.log("Open Camera Roll") }}
-                />
-                <SimpleInput
-                    reference={msgRef}
-                    placeholder="Say something"
-                    cStyle={{ overflow: "hidden", flex: 1, }}
-                    tStyle={styles.message}
-                    autoFocus={true}
-                    multiline={true}
-                    maxLength={300}
-                    keyboardAppearance="dark"
-                    onChangeText={(text) => {
-                        setMsg(text);
-                    } }
-                />
-                <IconButton
-                    icon="arrow-forward-circle"
-                    brand="Ionicons"
-                    color={colors.pBeam}
-                    size={34}
-                    style={{
-                        ...css.beamShadow,
-                        shadowColor: colors.pBeamShadow,
-                        marginBottom: 6,
-                    }}
-                    onPress={() => { if (debug) console.log("Send") }}
-                />
-            </View>
-            <View style={{ height: route.params.KeyboardHeight == 0 ? Keyboard.keyboardHeight - 24 : route.params.KeyboardHeight-24 }} />
+                </View>
+                <DarkBeam style={{ backgroundColor: colors.container, height: 1, marginBottom: 10}} />
+                <View style={styles.textBox}>
+                    <IconButton
+                        icon="camera"
+                        brand="Ionicons"
+                        color={colors.text3}
+                        style={{ marginBottom: 6, }}
+                        size={34}
+                        onPress={() => { if (debug) console.log("Open Camera") }}
+                    />
+                    <View style={{width: 10} } />
+                    <IconButton
+                        icon="add-circle"
+                        brand="Ionicons"
+                        color={colors.text3}
+                        size={34}
+                        style={{ marginBottom: 6, } }
+                        onPress={() => { if (debug) console.log("Open Camera Roll") }}
+                    />
+                    <SimpleInput
+                        reference={msgRef}
+                        placeholder="Say something"
+                        cStyle={{ overflow: "hidden", flex: 1, }}
+                        tStyle={styles.message}
+                        autoFocus={false}
+                        multiline={true}
+                        maxLength={300}
+                        keyboardAppearance="dark"
+                        onChangeText={(text) => {
+                            setMsg(text);
+                        } }
+                    />
+                    <IconButton
+                        icon="arrow-forward-circle"
+                        brand="Ionicons"
+                        color={colors.pBeam}
+                        size={34}
+                        style={{
+                            ...css.beamShadow,
+                            shadowColor: colors.pBeamShadow,
+                            marginBottom: 6,
+                        }}
+                        onPress={() => { if (debug) console.log("Send") }}
+                    />
+                </View>
+            </KeyboardAvoidingView>
 
 
         </Screen>
@@ -89,7 +169,8 @@ const styles = StyleSheet.create({
         maxHeight: 120,
     },
     chats: {
-        margin: 6,
+        justifyContent: "flex-start",
+        flex: 1
     },
     page: {
         justifyContent: "flex-end",
